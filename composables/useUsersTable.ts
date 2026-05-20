b1374e9254cb97caf1d1d33431cad77d8b70ab19
lenakononchuk <kononchuk.elena91@gmail.com>
2026-05-20 15:19:46 +0300

    implement useUsersTable hook for filtering, sorting and pagination

--- composables/useUsersTable.ts
+++ composables/useUsersTable.ts
@@ -1,4 +1,5 @@
 export function useUsersTable(users) {
+
   // filters
   const search = ref('')
   const role = ref(null)
@@ -11,13 +12,54 @@ export function useUsersTable(users) {
   const page = ref(1)
   const perPage = ref(10)
 
-  // TODO:
-  // - filteredUsers
-  // - sortedUsers
+  const filteredUsers = computed(() => {
+
+    const searchQuery = search.value.toLowerCase()
+
+    return users.filter((user) => {
+      const filterByName = user.name.toLowerCase().includes(searchQuery) || user.email.toLowerCase().includes(searchQuery);
+      const filterByRole = role.value === null || user.role === role.value
+      return filterByName && filterByRole
+    })
+  })
+  const sortedUsers = computed(() => {
+
+    if (sortBy.value === null) {
+      return filteredUsers.value
+    }
+    return [...filteredUsers.value].sort((a, b) => {
+
+      let valueA = a[sortBy.value];
+      let valueB = b[sortBy.value];
+
+      if (sortBy.value === 'createdAt') {
+        valueA = new Date(valueA)
+        valueB = new Date(valueB)
+
+      }
+
+      const result = valueA - valueB
+
+      return sortDirection.value === 'asc' ? result : -result
+    })
+
+  })
   // - paginatedUsers
-  // - totalPages
-  const paginatedUsers = null
-  const totalPages = null
+  const paginatedUsers = computed(() => {
+
+    const pageStart = (page.value - 1) * perPage.value;
+    const pageEnd = pageStart + perPage.value;
+
+    return sortedUsers.value.slice(pageStart, pageEnd)
+  })
+
+
+  const totalPages = computed(() =>{
+
+    const total = Math.ceil(filteredUsers.value.length / perPage.value)
+    return total === 0 ? 1 : total;
+  })
+
 
   return {
     search,
@@ -29,5 +71,6 @@ export function useUsersTable(users) {
 
     paginatedUsers,
     totalPages,
+
   }
 }
